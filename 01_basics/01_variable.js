99c44abab2028664d25b4409cec30a4f1c4b5533
BharatiJayanti <151519992+BharatiJayanti@users.noreply.github.com>
2024-07-07 11:09:33 +0000

    commit

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountNumber = 12345
+let accountEmail = "bharati@gmail.com"
+var accountPassword = "jksn"
+accountPassword = "vizag"
+var accountState;
+/*
+prefer let pver vr as it is not compatible for both block scope and functinol scope
+var iis a global scope
+*/
+accountEmail = "jk@gmail.com"
+accountPassword = "jk"
+accountCity = "sklm"
+console.table([accountNumber,accountEmail,accountPassword,accountCity,accountState])
