900f8b0f6588ca3e6d5d68865e00dcc6eb9486f9
BharatiJayanti <151519992+BharatiJayanti@users.noreply.github.com>
2024-07-09 01:23:28 +0000

    commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs,not broswer
+console.log(3+3) // code readdability
+console.log("Bharati")
+
+let nmae = "Bharati"
+let age = 18
+let LoggdedIn = false
+
+//primitive datatypes
+//number => 2 to power 53 
+//bigint => use only in large one lke stock markety,facebook etc
+//string => true/false
+//null => standlone value
+//undefined => the value is not assigned yet
+//symbol => used for uniquesss
+
+
+//object
+console.log(typeof "Bharati");
+console.log(typeof null);
+console.log(typeof undefined);
