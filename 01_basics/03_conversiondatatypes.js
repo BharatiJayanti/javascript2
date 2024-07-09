84da24474c8c4f81f4a34a6d1e663f10cfb463bc
BharatiJayanti <151519992+BharatiJayanti@users.noreply.github.com>
2024-07-09 01:55:14 +0000

    03_conversiondatatypes,js

---
+++ 01_basics/03_conversiondatatypes.js
@@ -0,0 +1,48 @@
+//let score = 33
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let score = "33"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+/*let score = "33abc"
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+/*let score = null
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+/*
+let score = true     //undefined
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+//"33" => 33
+//"33abc" => NAN
+//true => 1; false => 0
+
+/*let loggedIn = 1
+let booleanLoggedIn = Boolean(loggedIn)
+console.log(booleanLoggedIn)
+
+let loggesIn = 0
+let booleanLoggesIn = Boolean(loggesIn)
+console.log(booleanLoggesIn)*/
+
+/*let loggedIn = "Bharati"
+let booleanLoggedIn = Boolean(loggedIn)
+console.log(booleanLoggedIn)*/
+
+let score = 33
+let valueInNumber = String(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
