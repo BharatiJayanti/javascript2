b2a800de05c187d823a020b0882e10451b9b71cc
BharatiJayanti <151519992+BharatiJayanti@users.noreply.github.com>
2024-07-16 02:02:56 +0000

    datatypes_summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -11,7 +11,7 @@
 
 const id = Symbol('123')
 const anoId = Symbol('123')
-console.log(id === anoId);
+//console.log(id === anoId);
 
 const bigNumber = 345672576257625765n
 
@@ -28,11 +28,35 @@ const bigNumber = 345672576257625765n
  const myFunction = function(){
     console.log("hello world");
  }
- console.log(typeof id);
+ /*console.log(typeof id);
  console.log(typeof bigNumber);
  console.log(typeof myFunction);
  console.log(typeof bigNumber);
- console.log(typeof heros);
+ console.log(typeof heros);*/
  
 
  // javasceipt is dynamically type language0
+ //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ 
+ //  Stack(primitive), Heap(Non primitive)
+
+
+ let myYoutube = "Codehelp"
+ let anoid =  myYoutube
+anoid = "lovebabbar"
+console.log(myYoutube);
+console.log(anoid);
+
+
+//Non primitive
+let user1 = {
+   email: "1234@gmail.com",
+   uid: "123"
+}
+let user2 = user1
+console.log(user1);
+
+
+user2.email = "love.123"
+console.log(user2.email);
+console.log(user1.email);
