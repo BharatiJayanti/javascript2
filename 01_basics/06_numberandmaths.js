d6bdd2019e32e45a7725daaeefafe2dfd3f5828d
BharatiJayanti <151519992+BharatiJayanti@users.noreply.github.com>
2024-07-18 02:14:05 +0000

    numbersandmath

---
+++ 01_basics/06_numberandmaths.js
@@ -0,0 +1,23 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+//console.log(balance.toString());
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1)); // gives 0's after the decimal and the value in to fixed denotes how many places to be after decimal
+
+const otherNumber = 123.7894
+//console.log(otherNumber.toPrecision(4));
+
+const numbers = 1000000
+//console.log(numbers.toLocaleString());
+//console.log(numbers.toLocaleString('en-IN'));
+
+//++++++++++++++++maths++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-7));
+console.log(Math.round(7.6));
+
+
