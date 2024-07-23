842d010578a0cf677ace8e83f5522bbabb098dd9
BharatiJayanti <151519992+BharatiJayanti@users.noreply.github.com>
2024-07-23 02:19:59 +0000

    dateandtime

---
+++ 01_basics/07_dateandtime.js
@@ -0,0 +1,15 @@
+//Dates4
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+
+//let myCreatedDate = new Date(2023,0,23)
+// let myCreatedDate = new Date(2023,0,23,5,3)
+// console.log(myCreatedDate.toDateString());
+//let myCreatedDate = new Date("2024-07-23")
+let myCreatedDate = new Date("07-23-2024")
+console.log(myCreatedDate.toLocaleString());
