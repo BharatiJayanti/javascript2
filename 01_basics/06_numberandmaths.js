4c59a7af7e4b4ec674783d149e6723e3b87e14bf
BharatiJayanti <151519992+BharatiJayanti@users.noreply.github.com>
2024-07-23 01:33:34 +0000

    numberandmaths

--- 01_basics/06_numberandmaths.js
+++ 01_basics/06_numberandmaths.js
@@ -16,8 +16,22 @@ const numbers = 1000000
 
 //++++++++++++++++maths++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(-7));
-console.log(Math.round(7.6));
+//console.log(Math);
+//console.log(Math.round(7.6));
+//console.log(Math.ceil(7.6));
+//console.log(Math.min(2,3,6,8));
+//console.log(Math.abs(-7));
+//console.log(Math.max(2,3,6,8));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 20;
+const max = 30;
+
+console.log(Math.floor(Math.random()*(max-min +1)) *min);
+
+
 
 
