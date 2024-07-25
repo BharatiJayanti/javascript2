635fdf636d2ef64d8826082448ab64ab732941c8
BharatiJayanti <151519992+BharatiJayanti@users.noreply.github.com>
2024-07-25 01:34:46 +0000

    dateandtime

--- 01_basics/07_dateandtime.js
+++ 01_basics/07_dateandtime.js
@@ -12,4 +12,22 @@
 // console.log(myCreatedDate.toDateString());
 //let myCreatedDate = new Date("2024-07-23")
 let myCreatedDate = new Date("07-23-2024")
-console.log(myCreatedDate.toLocaleString());
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimestamp = Date.now()
+
+// console.log(myTimestamp);// for exact time
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));//converting in sec
+
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+
+newDate.toLocaleString('default ,{
+    weekday:"long",
+   
+})
+
