865a4bdabe74477894272a8d40d2e3319ee77dd4
BharatiJayanti <151519992+BharatiJayanti@users.noreply.github.com>
2024-09-23 06:44:49 +0000

    functions

--- 02_basics/01_functions.js
+++ 02_basics/01_functions.js
@@ -22,6 +22,30 @@ const result =addTwoNumbers(3,5)
 // console.log("Result:",result);
 
 function loginUserMessage(username){
-    return ${username} just logged in'
+    return `${username} just logged in`
 }
-console.log(loginUserMessage("bharati"))
+// console.log(loginUserMessage("bharati"))
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+console.log(calculateCartPrice(200,400,500,2000))
+const user={
+    username:"Bharati",
+    price: 200
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+handleObject(user)
+handleObject({
+    username:"ronny",
+    price : 500
+})
+const myNewArray =[200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,500,1000]));
