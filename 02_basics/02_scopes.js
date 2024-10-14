69da10e4a662ad2481cb63dd79642f7d416e2eba
BharatiJayanti <151519992+BharatiJayanti@users.noreply.github.com>
2024-10-14 06:49:52 +0000

    scope

--- 02_basics/02_scopes.js
+++ 02_basics/02_scopes.js
@@ -4,10 +4,25 @@ var c = 300
 if(true){
     let a = 10
     const b = 20
-    var c = 30
+    console.log("Inner:",a);
 }
 
 
 // console.log(a);
 // console.log(b);
 console.log(c);
+
+function one(){
+    const username = "bharati"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+
+    }
+    // console.log(website);
+
+    two()
+}
+
+one()
