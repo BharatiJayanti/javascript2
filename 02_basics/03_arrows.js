ea75e3350fa284715b84d2d570ef11c1d5536413
BharatiJayanti <151519992+BharatiJayanti@users.noreply.github.com>
2024-10-16 19:28:48 +0000

    arrow

--- 02_basics/03_arrows.js
+++ 02_basics/03_arrows.js
@@ -11,4 +11,41 @@ const user = {
 // user.username = "sammy"
 // user.welcomeMessage()
 
- console.log(this);
+ //console.log(this);
+
+//  function chai(){
+//     let username = "bharati"
+//     console.log(this.username);//this keyword i not workng in function
+//  }
+//  chai()
+
+// const chai = function (){
+//     let username = "bharati"
+//     console.log(this.username);
+
+ 
+// }
+const chai =  () => {
+    let username = "bharati"
+    console.log(this);
+    
+     
+    }
+
+
+//chai()
+
+// const addTwo = (num1,num2) =>{
+//     return num1 + num2
+// }
+// console.log(addTwo(3,4))
+
+// const addTwo = (num1,num2) => num1 + num2 //implicit return
+
+// const addTwo = (num1,num2) => (num1+num2)
+const addTwo = (num1,num2) => ({username:"bharati)"})
+
+console.log(addTwo(3,4))
+
+// const myArray = [2,5,3,7,8]
+// myArray.forEach()
