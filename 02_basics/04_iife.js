e12ea61a70f98c8819c35f974a5c75fa8dd75067
BharatiJayanti <151519992+BharatiJayanti@users.noreply.github.com>
2024-10-17 02:09:59 +0000

    iife

--- 02_basics/04_iife.js
+++ 02_basics/04_iife.js
@@ -2,5 +2,18 @@
 // we use iife to remove the pollution of global scope or its variable
 (function chai(){
     console.log(`DB CONNECTED`);
-})()
+})();
 // chai()
+
+// ( function aurcode()  {
+//     console.log(`DB CONNECTED TWO`);
+// }) (); //when we write two iife we have to put the semicolon in last
+
+// ( () => {
+//     console.log(`DB CONNECTED TWO`);
+// }) ();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+}) ('bharati')
+
