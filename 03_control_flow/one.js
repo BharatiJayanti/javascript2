c6fff3f749c6bb359d09a9c7a8edf3354fe35a8a
BharatiJayanti <151519992+BharatiJayanti@users.noreply.github.com>
2024-10-17 06:45:25 +0000

    one

--- 03_control_flow/one.js
+++ 03_control_flow/one.js
@@ -1,24 +1,47 @@
 // if
-const isUserloggedIn = true
-const temperature = 41
+// const isUserloggedIn = true
+// const temperature = 41
 
-if(temperature > 50){ // if true the inner code will be executed or if it is false then inner code is not executed
-      console.log("less than 50"); 
+// if(temperature > 50){ // if true the inner code will be executed or if it is false then inner code is not executed
+//       console.log("less than 50"); 
 
-}
-else{
-    console.log("Temperature is greater than 50");
-}
-console.log("execute");
+// }
+// else{
+//     console.log("Temperature is greater than 50");
+// }
+// console.log("execute");
 
 // <, >, <=, >=, ==, !=, ===, !==
 
-const score = 200
+// const score = 200
 
-if(score > 100){
-    const power = "fly"
-    console.log(`User power : $(power)`);
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User power : ${power}`);
+// }
+
+// console.log(`User power : ${power}`);
+
+//const balance = 1000
+// if (balance > 500) console.log("test"),console.log("test2"); don't do this
+
+// if (balance < 500){
+//     console.log("less than 500")
+
+// } else if (balance < 750){
+//     console.log("less than 750")
+// } else{
+//     console.log("lesss than 1200")
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
 }
-
-console.log(`User power : $(power)`);
-
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("user logged in");
+}
