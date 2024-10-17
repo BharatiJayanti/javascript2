0dd25212438183dd7ea1281647c3b38440ac09a2
BharatiJayanti <151519992+BharatiJayanti@users.noreply.github.com>
2024-10-17 18:22:23 +0000

    truth

--- 03_control_flow/truthy.js
+++ 03_control_flow/truthy.js
@@ -8,7 +8,7 @@ if(userEmail){
 
 // falsy values
 
-false, 0, -0, BigInt 0n, "", null, undefined, NaN
+//false, 0, -0, BigInt 0n, "", null, undefined, NaN
 
 //truthy values
 // "0",'false', " ", [], {}, function(){}
@@ -18,6 +18,26 @@ if (userEmail.length === 0) {
 }
 const emptyObj = {}
 
-if(Object.keys(emptyObj).length === 0) {
-    
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
 }
+// NUllish Coalescing Operator (??): null undefined
+
+let val1;
+//val1 = 5 ?? 10
+//val1 = null ?? 10
+//val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+
+
+
+
+console.log(val1);
+// Terniary Operator
+
+//condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("less than 80") : console.log
+("more than 80")
