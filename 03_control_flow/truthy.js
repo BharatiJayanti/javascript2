cefd3dc47d86d7bd0e75066bcfd3b119f6324921
BharatiJayanti <151519992+BharatiJayanti@users.noreply.github.com>
2024-10-17 07:35:04 +0000

    truth

---
+++ 03_control_flow/truthy.js
@@ -0,0 +1,11 @@
+const userEmail = "123@bharati.ai"
+
+if(userEmail){
+    console.log("Got the user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// falsy values
+
+false, 0, -0, BigInt 0n, "", null, undefined, NaN
