67210db571cafdb3408bef70123adc526f1e6bf8
BharatiJayanti <151519992+BharatiJayanti@users.noreply.github.com>
2024-10-17 06:30:33 +0000

    one

---
+++ 03_control_flow/one.js
@@ -0,0 +1,24 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+if(temperature > 50){ // if true the inner code will be executed or if it is false then inner code is not executed
+      console.log("less than 50"); 
+
+}
+else{
+    console.log("Temperature is greater than 50");
+}
+console.log("execute");
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+const score = 200
+
+if(score > 100){
+    const power = "fly"
+    console.log(`User power : $(power)`);
+}
+
+console.log(`User power : $(power)`);
+
