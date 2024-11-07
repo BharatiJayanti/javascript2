409900d17fadc5e0b6d3232be72b8eaf7d3d35fc
BharatiJayanti <151519992+BharatiJayanti@users.noreply.github.com>
2024-11-07 18:54:14 +0000

    six

---
+++ 04_iterations/six.js
@@ -0,0 +1,46 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+
+const values = coding.forEach ( (item) => {
+    //console.log(item);
+    return item
+})
+
+//console.log(values);
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter((num) => {
+//     return num > 4
+// })
+
+// const newNums = []
+//  myNums.forEach( (num) => {
+//     if(num > 4){
+//         newNums.push(num)
+//     }
+//  })
+
+
+// console.log(newNums);
+
+const userBooks = [
+    {title: 'Book one', genre: 'Non-Fiction', publish:1981,
+    edition:1989},
+    {title: 'Book one', genre: 'Non-Fiction', publish:1981,
+    edition:1989},
+    {title: 'Book one', genre: 'Non-Fiction', publish:1981,
+    edition:1989},
+    {title: 'Book one', genre: 'Non-Fiction', publish:1981,
+    edition:1989},
+    {title: 'Book one', genre: 'Non-Fiction', publish:1981,
+    edition:1989},
+    {title: 'Book one', genre: 'Non-Fiction', publish:1981,
+    edition:1989},
+    {title: 'Book one', genre: 'Non-Fiction', publish:1981,
+    edition:1989},
+
+
+
+
+]
